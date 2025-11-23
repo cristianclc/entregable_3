d98670dbf520acac941f35556ecad581597f2810
cristianclc <cristianclineroc@gmail.com>
2025-11-23 14:10:15 -0500

    Requirements

---
+++ versiones.py
@@ -0,0 +1,26 @@
+import sklearn
+import numpy
+import pandas
+import xgboost
+import dash
+import plotly
+import gunicorn
+import dash_bootstrap_components
+import joblib
+import psycopg2
+import sqlalchemy
+#import dotenv
+
+print("=== VERSIONES EXACTAS ===")
+print(f"scikit-learn: {sklearn.__version__}")
+print(f"numpy: {numpy.__version__}")
+print(f"pandas: {pandas.__version__}")
+print(f"xgboost: {xgboost.__version__}")
+print(f"dash: {dash.__version__}")
+print(f"plotly: {plotly.__version__}")
+print(f"gunicorn: {gunicorn.__version__}")
+print(f"dash-bootstrap-components: {dash_bootstrap_components.__version__}")
+print(f"joblib: {joblib.__version__}")
+print(f"psycopg2-binary: {psycopg2.__version__}")
+print(f"sqlalchemy: {sqlalchemy.__version__}")
+#print(f"python-dotenv: {dotenv.__version__}")
